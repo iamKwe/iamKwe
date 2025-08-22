49d1f06faf453f78849b0a2f74d0930622c5a999
Kwe <202508586771@alunos.estacio.br>
2025-08-21 23:02:36 -0300

    Update projeto1.c
    Adiciona as informações de cada etapa do projeto.

--- Portifólio/projeto1.c
+++ Portifólio/projeto1.c
@@ -4,8 +4,11 @@ int main(){
     int nota1, nota2, nota3;
     float media;
 
+
+
     printf("*** Programa de Cálculo de Média ***\n");
 
+ //Informar respectivas notas
     printf("Digite a nota de AV1: \n");
     scanf("%d", &nota1);
 
@@ -15,10 +18,12 @@ int main(){
     printf("Digite a nota do TRABALHO: \n");
     scanf("%d", &nota3);
 
+//Calcular a média do aluno
     media = (float)(nota1 + nota2 + nota3) / 3;
 
     printf("Sua média é:%.2f\n", media);
 
+//Informar a aprovação ou reprova do aluno
     if(media>=6)
         printf("Parabéns! Voce está aprovado!");
     else
