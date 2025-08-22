e499d339502097746dc36f2fd7b598d57209c130
Kwe <202508586771@alunos.estacio.br>
2025-08-21 22:31:20 -0300

    Projeto 1
    Primero projeto feito sozinho. Utilizei tudo que aprendi até o momento para produzir este breve projeto.
    
    O objetivo é calcular a média do aluno e informa-lo se foi APROVADO ou REPROVADO.

---
+++ Portifólio/projeto1.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main(){
+    int nota1, nota2, nota3;
+    float media;
+
+    printf("*** Programa de Cálculo de Média ***\n");
+
+    printf("Digite a nota de AV1: \n");
+    scanf("%d", &nota1);
+
+    printf("Digite a nota de AV2: \n");
+    scanf("%d", &nota2);
+
+    printf("Digite a nota do TRABALHO: \n");
+    scanf("%d", &nota3);
+
+    media = (float)(nota1 + nota2 + nota3) / 3;
+
+    printf("Sua média é:%.2f\n", media);
+
+    if(media>=6)
+        printf("Parabéns! Voce está aprovado!");
+    else
+        printf("Infelizmente voce reprovou. Mas nao desista!");
+    
+
+
+    return 0;
+
+}
