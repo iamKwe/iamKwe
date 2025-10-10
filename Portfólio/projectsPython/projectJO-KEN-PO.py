070896bb8cc6d6cee5e83df8f87f10cad67c4b4e
Kwe <202508586771@alunos.estacio.br>
2025-10-09 23:01:12 -0300

    Jogo de Jo-Ken-Po
    Criacao de um jogo, utilizando de arrays e loops para tornar o jogo interativo.

---
+++ Portfólio/projectsPython/projectJO-KEN-PO.py
@@ -0,0 +1,52 @@
+import random
+
+# Sistema do jogo
+
+def iniciarJogo():
+
+    itens = ('PEDRA', 'PAPEL', 'TESOURA')
+    escolhaComputador = random.choice(itens)
+
+    print("Escolha entre [PEDRA|PAPEL|TESOURA]")
+    escolhaJogador = str(input("> "))
+
+    if escolhaComputador == escolhaJogador:
+        print("Empatou!")
+    elif (escolhaComputador == "PEDRA" and escolhaJogador == "PAPEL") or (escolhaComputador == "PAPEL" and escolhaJogador == "TESOURA") or (escolhaComputador == "TESOURA" and escolhaJogador == "PEDRA"):
+        print("Voce ganhou!")
+    elif (escolhaComputador == "PEDRA" and escolhaJogador == "TESOURA") or (escolhaComputador == "TESOURA" and escolhaJogador == "PAPEL") or (escolhaComputador == "PAPEL" and escolhaJogador == "PEDRA"):
+        print("Voce perdeu!")
+    else:
+        print("Erro. Tente novamente.")
+
+    print("Jogador: ", escolhaJogador)
+    print("Computador: ", escolhaComputador)
+
+# Regras do Jogo
+
+def regrasJogo():
+    print("Regra 1: Nao jogar a mesma mao, mais de 2 vezes seguidas.")
+    print("Regra 2: Se divirta!")
+    print("Regra 3: Nao xingue o adversário.")
+
+
+# Menu do Jogo
+
+while True:
+    print("=== Bem-vindo ao Jo-Ken-Po ===")
+    print("1. Iniciar Jogo")
+    print("2. Regras")
+    print("3. Sair")
+    opcao = input("> ")
+
+    if opcao == "1":
+        iniciarJogo()
+
+    elif opcao == "2":
+        regrasJogo()
+
+    elif opcao == "3":
+        print("Saindo...")
+        break
+    else:
+        print("Erro. Tente novamente.")
